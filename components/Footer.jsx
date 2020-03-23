9ded7a9f1e870b01abaa55ec7853e00cb41a89e3
bdanzer <britt.danzer@gmail.com>
2020-03-22 22:44:29 -0500

    add google tracking

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,6 +1,16 @@
-import React from "react";
+import React, { useEffect } from "react";
+
+import { initGA, logPageView } from "../utils/google";
 
 export default function Footer() {
+    useEffect(() => {
+        if (!window.GA_INITIALIZED) {
+            initGA();
+            window.GA_INITIALIZED = true;
+        }
+        logPageView();
+    }, []);
+
     return (
         <>
             {/* <script
